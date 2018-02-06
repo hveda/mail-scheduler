9af40cfce0e45b02f883c1e03336fc466f93b1e7
Heri Rusmanto <retphern@gmail.com>
2018-02-07 00:08:32 +0700

    Delete old file

--- app/event/views.py
+++ app/event/views.py
@@ -1,53 +1,48 @@
-# project/items/views.py
+# app/event/views.py
 
 # IMPORTS
 from flask import render_template, Blueprint, request, redirect, url_for, flash, Markup
-from flask_login import current_user, login_required
-from project import db
-from project.models import Items, User
-from .forms import ItemsForm, EditItemsForm
+from app.database import db
+from app.database.models import Event, Recipient
+from .forms import EventsForm, EditEventsForm
 
 
 # CONFIG
-items_blueprint = Blueprint('items', __name__, template_folder='templates')
+blueprint = Blueprint('items', __name__, template_folder='templates')
 
 
 # ROUTES
-@items_blueprint.route('/all_items', methods=['GET', 'POST'])
-@login_required
-def all_items():
+@blueprint.route('/', methods=['GET', 'POST'])
+def all_events():
     """Render homepage"""
-    all_user_items = Items.query.filter_by(user_id=current_user.id)
-    return render_template('all_items.html', items=all_user_items)
+    all_events = Event.query.all()
+    return render_template('all_events.html', items=all_events)
 
 
-@items_blueprint.route('/add_item', methods=['GET', 'POST'])
-@login_required
-def add_item():
+@blueprint.route('/add_event', methods=['GET', 'POST'])
+def add_event():
     form = ItemsForm(request.form)
     if request.method == 'POST':
         if form.validate_on_submit():
             try:
-                new_item = Items(form.name.data, form.notes.data,
-                                 current_user.id)
-                db.session.add(new_item)
+                new_event = Event(form.data)
+                db.session.add(new_event)
                 db.session.commit()
                 message = Markup(
-                    "<strong>Well done!</strong> Item added successfully!")
+                    "<strong>Well done!</strong> Event added successfully!")
                 flash(message, 'success')
-                return redirect(url_for('home'))
+                return redirect(url_for('all_events'))
             except:
                 db.session.rollback()
                 message = Markup(
-                    "<strong>Oh snap!</strong>! Unable to add item.")
+                    "<strong>Oh snap!</strong>! Unable to add event.")
                 flash(message, 'danger')
-    return render_template('add_item.html', form=form)
+    return render_template('add_event.html', form=form)
 
 
-@items_blueprint.route('/edit_item/<items_id>', methods=['GET', 'POST'])
-@login_required
-def edit_item(items_id):
-    form = EditItemsForm(request.form)
+@blueprint.route('/edit_event/<event_id>', methods=['GET', 'POST'])
+def edit_event(event_id):
+    form = EditEventForm(request.form)
     item_with_user = db.session.query(Items, User).join(User).filter(Items.id == items_id).first()
     if item_with_user is not None:
         if current_user.is_authenticated and item_with_user.Items.user_id == current_user.id:
@@ -58,15 +53,15 @@ def edit_item(items_id):
                         item.name = form.name.data
                         item.notes = form.notes.data
                         db.session.commit()
-                        message = Markup("Item edited successfully!")
+                        message = Markup("Event edited successfully!")
                         flash(message, 'success')
                         return redirect(url_for('home'))
                     except:
                         db.session.rollback()
                         message = Markup(
-                            "<strong>Error!</strong> Unable to edit item.")
+                            "<strong>Error!</strong> Unable to edit event.")
                         flash(message, 'danger')
-            return render_template('edit_item.html', item=item_with_user, form=form)
+            return render_template('edit_event.html', item=item_with_user, form=form)
         else:
             message = Markup(
                 "<strong>Error!</strong> Incorrect permissions to access this item.")
@@ -76,8 +71,7 @@ def edit_item(items_id):
         flash(message, 'danger')
     return redirect(url_for('home'))
 
-@items_blueprint.route('/delete_item/<items_id>', methods=['GET', 'POST'])
-@login_required
+@blueprint.route('/delete_item/<items_id>', methods=['GET', 'POST'])
 def delete_item(items_id):
     item_with_user = db.session.query(Items, User).join(User).filter(Items.id == items_id).first()
     if item_with_user is not None:
